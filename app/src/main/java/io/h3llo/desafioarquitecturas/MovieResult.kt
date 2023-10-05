4e160c5b8610a916d201a7098c7482598cb64b19
beto <betolix@gmail.com>
2023-10-04 21:28:57 -0400

    Initial commit

---
+++ app/src/main/java/io/h3llo/desafioarquitecturas/MovieResult.kt
@@ -0,0 +1,8 @@
+package io.h3llo.desafioarquitecturas
+
+data class MovieResult(
+    val page: Int,
+    val results: List<ServerMovie>,
+    val total_pages: Int,
+    val total_results: Int
+)
---
+++ app/src/main/java/io/h3llo/desafioarquitecturas/MoviesService.kt
@@ -0,0 +1,9 @@
+package io.h3llo.desafioarquitecturas
+
+import retrofit2.http.GET
+
+interface MoviesService {
+
+    @GET("discover/movie?api_key=2daaecb9ca6189631bfadd5744bf366c")
+    suspend fun getMovies(): MovieResult
+}
