35bd366201fac53099b8bf706d581d6aa2d6d398
beto <betolix@gmail.com>
2023-10-09 01:04:35 -0400

    DIA DOS DEL RETO. EXTRAER LOGICA DE LA ACTIVITY AL VIEWMODEL

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,8 +64,11 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     // coil
     implementation("io.coil-kt:coil-compose:2.2.2")
-
-
+    // viewModel compose
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    // Para utilizar el observeAsState
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
+    implementation("androidx.compose.runtime:runtime-livedata:1.5.3")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/io/h3llo/desafioarquitecturas/MainActivity.kt
+++ app/src/main/java/io/h3llo/desafioarquitecturas/MainActivity.kt
@@ -1,10 +1,13 @@
 package io.h3llo.desafioarquitecturas
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
@@ -14,7 +17,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -22,10 +29,16 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import io.h3llo.desafioarquitecturas.ui.theme.DesafioArquitecturasTheme
 import retrofit2.Retrofit
@@ -39,17 +52,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             DesafioArquitecturasTheme {
 
-                val movies = produceState<List<ServerMovie>>(initialValue = emptyList()){
-                    value = Retrofit.Builder()
-                        .baseUrl("https://api.themoviedb.org/3/")
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build()
-                        .create(MoviesService::class.java)
-                        .getMovies()
-                        .results
-                        // .forEach{ Log.d("RetrofitRequest", it.toString())}
-                }
-
+                val viewModel :MainViewModel = viewModel()
+                val state by viewModel.state.collectAsState()
 
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -59,16 +63,29 @@ class MainActivity : ComponentActivity() {
                     Scaffold(
                         topBar = { TopAppBar(title = { Text(text = "Movies") }) }
                     ){ padding ->
-                        LazyVerticalGrid(
-                            columns = GridCells.Adaptive(120.dp),
-                            modifier = Modifier.padding(padding),
-                            horizontalArrangement = Arrangement.spacedBy(4.dp),
-                            verticalArrangement = Arrangement.spacedBy(4.dp),
-                            contentPadding = PaddingValues(4.dp)
-                        ){
-                            items(movies.value){ movie->
-                                MovieItem(movie)
+                        if(state.loading){
+                            Box(
+                                modifier = Modifier.fillMaxSize(),
+                                contentAlignment = Alignment.Center){
+                                CircularProgressIndicator()
                             }
+                        }
+                        if(state.movies.isNotEmpty()){
+                            LazyVerticalGrid(
+                                columns = GridCells.Adaptive(120.dp),
+                                modifier = Modifier.padding(padding),
+                                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                                verticalArrangement = Arrangement.spacedBy(4.dp),
+                                contentPadding = PaddingValues(4.dp)
+                            ){
+                                items(state.movies){ movie->
+                                    MovieItem(
+                                        movie = movie,
+                                        onClick = {viewModel.onMovieClick(movie)}
+                                    )
+                                }
+                            }
+
                         }
 
                     }
@@ -79,15 +96,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MovieItem(movie: ServerMovie) {
-    Column {
-        AsyncImage(
-            model = "https://image.tmdb.org/t/p/w185/${movie.poster_path}",
-            contentDescription = movie.title,
-            modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(2 / 3f)
-        )
+fun MovieItem(movie: ServerMovie, onClick: ()-> Unit ) {
+    Column(
+        modifier = Modifier.clickable(onClick = onClick )
+    ){
+        Box {
+
+            AsyncImage(
+                model = "https://image.tmdb.org/t/p/w185/${movie.poster_path}",
+                contentDescription = movie.title,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(2 / 3f)
+            )
+            if(movie.favorite){
+                Icon(
+                    imageVector = Icons.Default.Favorite,
+                    contentDescription = movie.title,
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(8.dp),
+                    tint = Color.Yellow
+                )
+            }
+        }
+
         Text(
             text = movie.title,
             modifier = Modifier.padding(16.dp),
